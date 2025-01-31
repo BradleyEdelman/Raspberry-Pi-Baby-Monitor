676a6a16d106d87ff52b180fd3363e48b30b1dfa
BradleyEdelman <bjedelma@gmail.com>
2025-01-31 13:24:53 +0100

    stream static image

--- src/web_server.py
+++ src/web_server.py
@@ -4,28 +4,10 @@ import subprocess
 app = Flask(__name__)
 
 def gen_video():
-    print("Starting video stream...")
-    command = [
-        "libcamera-vid",
-        "-t", "0",  # infinite time, continuous stream
-        "--inline",  # inline MJPEG frames
-        "--bitrate", "2000000",  # set bitrate for quality
-        "-o", "-",  # output to stdout (pipe)
-    ]
-    
-    proc = subprocess.Popen(command, stdout=subprocess.PIPE)
-    
-    print("Process started, reading frames...")
-    while True:
-        frame = proc.stdout.read(1024)
-        if frame:
-            print("Sending frame...")
-            yield (b'--frame\r\n'
-                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
-        else:
-            print("No frame received, stopping.")
-            break
-
+    with open("frame.jpg", "rb") as f:
+        frame = f.read()
+    yield (b'--frame\r\n'
+           b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
 @app.route('/')
 def index():
