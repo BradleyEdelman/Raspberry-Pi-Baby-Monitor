572a3cb53bfc9ffcf83e81abe12b7668a6015b6b
BradleyEdelman <bjedelma@gmail.com>
2025-02-03 13:29:52 +0100

    basicssss

--- src/stream_LED.py
+++ src/stream_LED.py
@@ -16,17 +16,14 @@ picam2.preview_configuration.main.format = "RGB888"
 picam2.configure("preview")
 picam2.start()
 
-# Initialize LED state
-led_state = "auto"  # Options: "auto", "on", "off"
-
 def generate_frames():
     while camera_streaming:
         frame = picam2.capture_array()
         _, buffer = cv2.imencode('.jpg', frame)  # Convert to JPEG format
         frame_bytes = buffer.tobytes()
         yield (b'--frame\r\n'
-            b'Content-Type: image/jpeg\r\n\r\n' + frame_bytes + b'\r\n')
-        time.sleep(0.05)  # Frame rate?
+               b'Content-Type: image/jpeg\r\n\r\n' + frame_bytes + b'\r\n')
+        time.sleep(0.05)  # Frame rate? 
 
 # Route to start or stop the stream
 @app.route('/toggle_stream')
@@ -40,6 +37,9 @@ def toggle_stream():
 def video_feed():
     return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
+# Initialize LED state
+led_state = "auto"  # Options: "auto", "on", "off"
+
 # # Route to control LED state
 # @app.route('/set_led/<state>')
 # def set_led(state):
@@ -52,26 +52,17 @@ def video_feed():
 # "design" the webpage
 @app.route('/')
 def index():
-    return render_template_string("""
+    return render_template_string(""" 
         <html>
-            <head>
-                <title>Raspberry Pi Video Stream</title>
-            </head>
+            <head><title>Raspberry Pi Video Stream</title></head>
             <body>
                 <h1>Raspberry Pi Video Stream</h1>
                 <img src="/video_feed" width="640" height="480">
-                
-                <h3>LED Control</h3>
-                <button onclick="window.location.href='/set_led/auto'">Auto LED</button>
-                <button onclick="window.location.href='/set_led/on'">LED On</button>
-                <button onclick="window.location.href='/set_led/off'">LED Off</button>
-
                 <h3>Camera Stream Control</h3>
                 <button onclick="window.location.href='/toggle_stream'">Start/Stop Stream</button>
             </body>
         </html>
     """)
 
-
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=False)
